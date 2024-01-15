6a5843625e57b2c5afbe155d1a7f898f73610ced
RomainCrz <101673637+RomainCrz@users.noreply.github.com>
2024-01-15 16:24:50 +0100

    started adding customer

--- src/pages/customer/AddCustomerButton.tsx
+++ src/pages/customer/AddCustomerButton.tsx
@@ -1,7 +1,73 @@
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { FormEventHandler } from "react";
 
 export type AddCustomerButtonProps = {};
 
 export const AddCustomerButton = (props: AddCustomerButtonProps) => {
-  return <Button>Add Customer</Button>;
+  const handleSubmit = (event: React.FormEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Add customer</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]" onSubmit={handleSubmit}>
+        <DialogHeader>
+          <DialogTitle>Add a new customer</DialogTitle>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="name" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="email" className="text-right">
+              Email
+            </Label>
+            <Input id="email" value="name@gmail.com" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="phone" className="text-right">
+              Phone
+            </Label>
+            <Input id="phone" value="0634035685" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="address" className="text-right">
+              Address
+            </Label>
+            <Input id="address" value="6 rue de la resistance" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="city" className="text-right">
+              City
+            </Label>
+            <Input id="city" value="Dijon" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="state" className="text-right">
+              State
+            </Label>
+            <Input id="state" value="Cote d'or" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="country" className="text-right">
+              Country
+            </Label>
+            <Input id="country" value="France" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 };
