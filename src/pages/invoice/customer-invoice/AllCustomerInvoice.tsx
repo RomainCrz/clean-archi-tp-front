02dbd676bf0b8c01b156151fe9a8f9024e64ce08
RomainCrz <101673637+RomainCrz@users.noreply.github.com>
2024-01-18 09:00:09 +0100

    Add AllCustomerInvoice and CreateInvoiceModal components

---
+++ src/pages/invoice/customer-invoice/AllCustomerInvoice.tsx
@@ -0,0 +1,59 @@
+import { Customer } from "schema/customerSchema";
+import { useGetAllInvoiceByUser } from "../hooks/useGetAllInvoiceByUser";
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { CustomerInvoiceTable } from "../CustomerInvoiceTable";
+import { Invoice } from "schema/invoiceSchema";
+import React from "react";
+import { toast } from "sonner";
+import { useUpdateCustomerInvoices } from "../hooks/useUpdateCustomerInvoice";
+
+export type AllCustomerInvoiceProps = {
+  customer: Customer;
+};
+
+export const AllCustomerInvoice = (props: AllCustomerInvoiceProps) => {
+  const { customer } = props;
+
+  const [newInvoices, setNewInvoices] = React.useState<Invoice[]>([]);
+
+  const { data, isLoading, isError } = useGetAllInvoiceByUser(customer.id || "");
+  const { mutate, isPending } = useUpdateCustomerInvoices(customer.id || "");
+
+  const handleSaveChanges = () => {
+    if (newInvoices.length === 0) {
+      toast.info("No changes to save");
+      return;
+    }
+
+    mutate(newInvoices);
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline" className="w-36">
+          See invoices
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[800px]">
+        <DialogHeader>
+          <DialogTitle>Customer invoices</DialogTitle>
+        </DialogHeader>
+        <CustomerInvoiceTable invoices={data || []} setNewInvoices={setNewInvoices} />
+        <DialogFooter>
+          <Button onClick={handleSaveChanges}>{isPending ? "Saving ..." : "Save changes"}</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
---
+++ src/pages/invoice/customer-invoice/CreateInvoiceModal.tsx
@@ -0,0 +1,78 @@
+import { getAllProduct } from "@/api/product/getAllProduct";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { useQuery } from "@tanstack/react-query";
+import { Customer } from "schema/customerSchema";
+import { useGetAllProducts } from "../hooks/useGetAllProducts";
+import { ProductsCombo } from "../components/ProductsCombo";
+import { Product } from "schema/productSchema";
+import React from "react";
+import { InvoiceProductsTable } from "../InvoiceProductsTable";
+import { useCreateInvoice } from "../hooks/useCreateInvoice";
+import { Invoice } from "schema/invoiceSchema";
+
+export type CreateInvoiceModalProps = {
+  customer: Customer;
+};
+
+export const CreateInvoiceModal = (props: CreateInvoiceModalProps) => {
+  const { customer } = props;
+
+  const { data, isLoading, isError } = useGetAllProducts();
+  const { mutate, isPending } = useCreateInvoice(customer.id || "");
+
+  const [invoiceProducts, setInvoiceProducts] = React.useState<Product[]>([]);
+
+  const handleSelectProduct = (product: Product) => {
+    setInvoiceProducts([...invoiceProducts, product]);
+  };
+
+  const handleCreateInvoice = () => {
+    const today = new Date();
+    const dueDate = new Date();
+    dueDate.setDate(today.getDate() + 30);
+
+    const totalAmount = invoiceProducts.reduce((acc, product) => acc + product.price, 0);
+
+    const totalTax = invoiceProducts.reduce((acc, product) => acc + product.tax, 0);
+
+    const invoice: Invoice = {
+      customer: customer,
+      products: invoiceProducts,
+      invoiceNumber: "INV-" + Math.floor(Math.random() * 1000000),
+      invoiceDate: today,
+      dueDate: dueDate,
+      status: "sent",
+      totalAmount: totalAmount,
+      totalTax: totalTax,
+      totalAmountWithTax: totalAmount + totalTax,
+    };
+
+    mutate(invoice);
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={"outline"} className="w-36">
+          Create invoice
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[700px]">
+        <DialogHeader>
+          <DialogTitle>Create invoice for {customer.name}</DialogTitle>
+        </DialogHeader>
+        {isLoading && <div>Loading...</div>}
+        {isError && <div>Error</div>}
+
+        <div>
+          <ProductsCombo products={data || []} handleSelectProduct={handleSelectProduct} />
+          <InvoiceProductsTable products={invoiceProducts} setInvoiceProducts={setInvoiceProducts} />
+        </div>
+        <DialogFooter>
+          <Button onClick={handleCreateInvoice}>{isPending ? "Creating..." : "Create"}</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
